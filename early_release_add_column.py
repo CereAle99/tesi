509a157d3bb03968987276959200cf6d8b32a894
Ale <alessandro@ceresi.eu>
2023-09-15 14:34:04 +0200

    add a column to the clinical_data file with the early relapse index and save it in a new file clinical_data_early_relapse.csv

--- early_release_add_column.py
+++ early_release_add_column.py
@@ -1,11 +1,17 @@
 import pandas as pd
 
-# Specifica il percorso del tuo file CSV
-file_path = '.\data\clincal_data_300523.xlsx'
 
-# Carica il file CSV in un DataFrame
-df = pd.read_csv(file_path)
 
-# Ora puoi lavorare con i dati nel DataFrame
-# Ad esempio, per stampare le prime 5 righe:
-print(df.head())
+# path of clinical_data_300523.xlsx
+file_path = './data/clincal_data_300523.xlsx'
+
+# load the file in a pandas dataframe
+clinical_data = pd.read_excel(file_path, engine='openpyxl')
+
+# add a column with the early relapse index
+clinical_data['early_relapse'] = ((clinical_data['PFS_I_EVENT'] == 1) & (clinical_data['PFS_I_MONTHS'] <= 12)).astype(int)
+
+# save it back to the file clinical_data_early_relapse.csv
+file_path = './data/clinical_data_early_relapse.csv'
+clinical_data.to_csv(file_path, index=False)
+
