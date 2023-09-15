e1def8b22290457279520d73c40e66115275874f
Ale <alessandro@ceresi.eu>
2023-09-15 13:16:13 +0200

    specified the filename of the data

--- early_release_add_column.py
+++ early_release_add_column.py
@@ -1,7 +1,7 @@
 import pandas as pd
 
 # Specifica il percorso del tuo file CSV
-file_path = ''
+file_path = '.\data\clincal_data_300523.xlsx'
 
 # Carica il file CSV in un DataFrame
 df = pd.read_csv(file_path)
