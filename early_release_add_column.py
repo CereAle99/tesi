5b84eab65deb3119a99e6b718807bfee9ba25621
Ale <alessandro@ceresi.eu>
2023-09-15 13:14:24 +0200

    lines to read the xlsx file into pandas object

---
+++ early_release_add_column.py
@@ -0,0 +1,11 @@
+import pandas as pd
+
+# Specifica il percorso del tuo file CSV
+file_path = ''
+
+# Carica il file CSV in un DataFrame
+df = pd.read_csv(file_path)
+
+# Ora puoi lavorare con i dati nel DataFrame
+# Ad esempio, per stampare le prime 5 righe:
+print(df.head())
