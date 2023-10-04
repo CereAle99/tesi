8cb4c7110f63828732ba82230b687bd3013c521e
CereAle99 <alessandro@ceresi.eu>
2023-10-04 17:23:18 +0200

    added the function for the find the cylinder which inscribes the spine but for now it's empty

--- segmentation_processing/spine_outofthe_cylinder.py
+++ segmentation_processing/spine_outofthe_cylinder.py
@@ -2,7 +2,6 @@ import nibabel as nib
 from scipy import ndimage
 import numpy as np
 import os
-from close_spine_area import dilate_spine
 
 # Get the present directory path and data directory
 current_directory = os.getcwd()
@@ -11,6 +10,12 @@ data_path = current_directory + '/data/'
 # Load the NIfTI segmentation and put it into a numpy array
 img = nib.load(data_path + "Spine.nii.gz")
 image = img.get_fdata()
+
+
+def spine_as_cylinder(data):
+
+    final_data = data
+    return final_data
 
 
 # Save the modified segmentation as a NIfTI file
