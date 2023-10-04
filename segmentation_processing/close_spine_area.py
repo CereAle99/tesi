9f88346d2e84d9e3f1aaa7ad01e2bd72144a84fb
CereAle99 <alessandro@ceresi.eu>
2023-10-04 17:03:33 +0200

    added the function which dilates fill the spine and dilates the segmentation area

--- segmentation_processing/close_spine_area.py
+++ segmentation_processing/close_spine_area.py
@@ -2,6 +2,7 @@ import nibabel as nib
 from scipy import ndimage
 import numpy as np
 import os
+from fill_spine import fill_spinal_holes
 
 # Get the present directory path and data directory
 current_directory = os.getcwd()
@@ -9,8 +10,27 @@ data_path = current_directory + '/data/'
 
 # Load the NIfTI segmentation and put it into a numpy array
 img = nib.load(data_path + "Spine.nii.gz")
-data = img.get_fdata()
+image = img.get_fdata()
+
+
+def dilate_spine(data, iterations=1):
+    """
+
+    Args:
+        data:
+        iterations:
+
+    Returns:
+
+    """
+
+    data = fill_spinal_holes(data, 3)
+    final_data = ndimage.binary_dilation(data, iterations=iterations)
+    return final_data
+
+
+final_image = fill_spinal_holes(image, 3)
 
 # Save the modified segmentation as a NIfTI file
 modified_img = nib.Nifti1Image(final_image, img.affine, img.header)
-nib.save(modified_img, os.path.join(data_path, 'show_results.nii.gz'))
+nib.save(modified_img, os.path.join(data_path, 'close_spine.nii.gz'))
