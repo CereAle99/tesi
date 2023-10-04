e02c5650f347cfd5df55a143d6f966ff96a4d16d
CereAle99 <alessandro@ceresi.eu>
2023-10-04 15:44:30 +0200

    added an algorithm that fills the holes in the spine segmentation

--- segmentation_processing/fill_spine.py
+++ segmentation_processing/fill_spine.py
@@ -1,4 +1,5 @@
 import nibabel as nib
+from scipy import ndimage
 import numpy as np
 import os
 
@@ -6,8 +7,35 @@ import os
 current_directory = os.getcwd()
 data_path = current_directory + '/data/'
 
+# Load the NIfTI segmentation and put it into a numpy array
 img = nib.load(data_path + "Spine.nii.gz")
 data = img.get_fdata()
 
-img = nib.Nifti1Image(data, np.eye(4))
-nib.save(img, os.path.join(data_path, 'show_results.nii.gz'))
+# Fill holes process
+
+# Set up the kernel for the fill process and the parameters
+n = 3
+kernel = np.array([
+                         [[0, 0, 0],
+                          [1, 1, 1],
+                          [0, 0, 0]],
+
+                         [[0, 0, 0],
+                          [1, 1, 1],
+                          [0, 0, 0]],
+
+                         [[0, 0, 0],
+                          [1, 1, 1],
+                          [0, 0, 0]]
+                        ],
+                  dtype=np.uint8)
+
+# dilation, fill holes and erosion
+modified_data = ndimage.binary_dilation(data, iterations=n)
+modified_data = ndimage.binary_fill_holes(modified_data, structure=kernel).astype(int)
+final_data = ndimage.binary_erosion(modified_data, iterations=n)
+
+
+# Save the modified segmentation as a NIfTI file
+modified_img = nib.Nifti1Image(final_data, img.affine, img.header)
+nib.save(modified_img, os.path.join(data_path, 'show_results_n=0.nii.gz'))
