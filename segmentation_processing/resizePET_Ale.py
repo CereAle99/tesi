5b9a8413065cde262a9504bfe6b45f22614e73cd
CereAle99 <alessandro@ceresi.eu>
2023-10-05 17:17:17 +0200

    use a scypy.ndimage zoom function to resize and modify the file header and affine. Not working

--- segmentation_processing/resizePET_Ale.py
+++ segmentation_processing/resizePET_Ale.py
@@ -1,32 +1,60 @@
 import numpy as np
 import nibabel as nib
+from scipy.ndimage import zoom
 import os
 
-if __name__=="__main__":
+if __name__ == "__main__":
     # All patients path
-    path = "data"
+    path = "data/test_PET"
 
     for folder in os.listdir(path):
-        try:
-            print("Patient: ", folder)
-            # Get label path
-            patient_path = os.path.join(path, folder)
-            image_path = os.path.join(patient_path, "PET/PT.nii")
+        print("Patient: ", folder)
+        # Get label path
+        patient_path = os.path.join(path, folder)
+        image_path = os.path.join(patient_path, "PT.nii")
 
-            # Load label
-            img_obj = nib.load(image_path)
-            img = img_obj.get_fdata()
-            # Transform label
-            img = np.resize(img, [512, 512, 239])
-            img_resized = nib.Nifti1Image(img, img_obj.affine, img_obj.header)
+        # Load label
+        img_obj = nib.load(image_path)
+        img = img_obj.get_fdata()
+        print("Initial shape: ", img.shape)
+        print(img_obj.header)
+        print(img_obj.affine)
 
-            # Save label
-            save_path = os.path.join(patient_path, "PET-Resized")
-            os.makedirs(save_path, exist_ok=True)
-            if not os.path.isfile(save_path+'/PT.nii'):
-                print("Saving PET")
-                nib.save(img_resized, save_path+'/PT.nii')
-        except: pass
+        # Evaluate the ratio of the images dimension
+        CT_PET_ratio = np.array([512, 512, 239]) / np.array(img.shape)
 
+        # Transform label
+        img = zoom(img, zoom=CT_PET_ratio)
+        print("Final shape: ", img.shape)
 
+        # Modify file header and affine
+        new_header = img_obj.header
+        new_header['dim'] = [3, 512, 512, 239, 1, 1, 1, 1]
+        new_header['pixdim'] = [1.,
+                                5.46875/CT_PET_ratio[0],
+                                5.46875/CT_PET_ratio[1],
+                                3.27/CT_PET_ratio[2],
+                                0.,
+                                0.,
+                                0.,
+                                0.]
+        new_header['srow_x'] = [-5.46875/CT_PET_ratio[0], 0., 0., 347.26562]
+        new_header['srow_y'] = [0., -5.46875/CT_PET_ratio[1], 0., 347.26562]
+        new_header['srow_z'] = [0., 0., 3.27/CT_PET_ratio[2], -771.7]
+        new_affine = np.array([
+                                [-5.46875 / CT_PET_ratio[0], 0., 0., 347.26562],
+                                [0., -5.46875 / CT_PET_ratio[1], 0., 347.26562],
+                                [0., 0., 3.27 / CT_PET_ratio[2], -771.7],
+                                [0., 0., 0., 1.]
+                            ])
+
+        img_resized = nib.Nifti1Image(img, new_affine, header=new_header)
+        print(img_resized.header)
+
+        # Save label
+        save_path = os.path.join(patient_path, "PET-Resized")
+        os.makedirs(save_path, exist_ok=True)
+        if not os.path.isfile(save_path + '/PT.nii'):
+            print("Saving PET")
+            nib.save(img_resized, save_path + '/PT.nii')
 
