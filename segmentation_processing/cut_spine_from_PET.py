6f08e269681c908a835e5a260bbdff453388040e
CereAle99 <alessandro@ceresi.eu>
2023-10-11 14:40:01 +0200

    add a function that cut the pet in the shape of the segmentation

---
+++ segmentation_processing/cut_spine_from_PET.py
@@ -0,0 +1,54 @@
+import nibabel as nib
+import numpy as np
+import os
+from resizePET_Ale import pet_segmentation_make_compatible
+
+# Get the present directory path and data directory
+current_directory = os.getcwd()
+data_path = current_directory + '/data/'
+image_path = data_path + 'test_PET/MPC_2_20110413/'
+
+# Load the segmentation NIfTI file
+spine = nib.load(data_path + "Spine.nii.gz")
+
+# Load the segmentation NIfTI file
+image_obj = nib.load(image_path + "PT.nii")
+
+
+def cut_spine_shape(input_image, mask):  # whether the dim are the same will be treated when resize function is complete
+    """
+
+    Args:
+        input_image:
+        mask:
+
+    Returns:
+
+    """
+
+    # Put the segmentation into a numpy array
+    spine_mask = mask.get_fdata()
+
+    # Put the image into a numpy array
+    image = input_image.get_fdata()
+
+    # Make the mask composed by zero and ones and multiply it to the input image
+    mask_01 = spine_mask
+    mask_01[mask_01 < 41.] = 0
+    mask_01[mask_01 >= 41.] = 1
+    cut_image = image * mask_01
+
+    return cut_image
+
+
+# Cut the image
+# final_image = cut_spine_shape(image_obj, spine)
+
+
+# Save the modified segmentation as a NIfTI file
+# modified_img = nib.Nifti1Image(final_image, image_obj.affine, image_obj.header)
+# nib.save(modified_img, os.path.join(data_path, 'spine_PET.nii'))
+
+pet_image, ct_image = pet_segmentation_make_compatible(image_obj, spine, True)
+nib.save(pet_image, os.path.join(data_path, 'PET_compatibility.nii'))
+nib.save(ct_image, os.path.join(data_path, 'spine_compatibility.nii'))
