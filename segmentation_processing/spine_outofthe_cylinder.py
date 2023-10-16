077e07b7ca0704c84ee612100ae88aaad8d680b1
CereAle99 <alessandro@ceresi.eu>
2023-10-16 15:09:00 +0200

    make the function work with the whole NIfTI file

--- segmentation_processing/spine_outofthe_cylinder.py
+++ segmentation_processing/spine_outofthe_cylinder.py
@@ -8,31 +8,32 @@ current_directory = os.getcwd()
 data_path = current_directory + '/data/'
 
 # Load the NIfTI segmentation and put it into a numpy array
-img = nib.load(data_path + "Spine.nii.gz")
-image = img.get_fdata()
+original_file = nib.load(data_path + "Spine.nii.gz")
 
 
-def spine_as_cylinder(input_data, close_spine=3):
+def spine_as_cylinder(input_nifti, close_spine=3):
     """
 
     Args:
-        input_data:
+        input_nifti:
         close_spine:
 
     Returns:
 
     """
 
-    data = input_data
-
     if close_spine != 0:
-        data = dilate_spine(data, close_spine)
+        dilated_nifti = dilate_spine(input_nifti, close_spine)
+    else:
+        dilated_nifti = input_nifti
+
+    image = dilated_nifti.get_fdata()
 
     [i_max, i_min, j_max, j_min, k_max, k_min] = [0, 999, 0, 999, 0, 999]
-    for i in range(data.shape[0]):
-        for j in range(data.shape[1]):
-            for k in range(data.shape[2]):
-                if data[i, j, k] != 0:
+    for i in range(image.shape[0]):
+        for j in range(image.shape[1]):
+            for k in range(image.shape[2]):
+                if image[i, j, k] != 0:
                     i_max, i_min = np.maximum(i_max, i), np.minimum(i_min, i)
                     j_max, j_min = np.maximum(j_max, j), np.minimum(j_min, j)
                     k_max, k_min = np.maximum(k_max, k), np.minimum(k_min, k)
@@ -40,18 +41,19 @@ def spine_as_cylinder(input_data, close_spine=3):
     cylinder_center = np.array([(i_max + i_min)*0.5, (j_max + j_min)*0.5])
     cylinder_radius = np.maximum((i_max - i_min)*0.5, (j_max - j_min)*0.5)
 
-    for i in range(data.shape[0]):
-        for j in range(data.shape[1]):
+    for i in range(image.shape[0]):
+        for j in range(image.shape[1]):
             point = np.array([i, j])
             if np.linalg.norm(cylinder_center - point) < cylinder_radius:
-                data[i, j, k_min:k_max+1] = 41.0
+                image[i, j, k_min:k_max+1] = 1
 
-    final_data = data
-    return final_data
+    # Put the image in a NIfTI file
+    final_nifti = nib.Nifti1Image(image, input_nifti.affine, input_nifti.header)
+
+    return final_nifti
 
 
-final_image = spine_as_cylinder(image, 3)
+modified_file = spine_as_cylinder(original_file, 3)
 
 # Save the modified segmentation as a NIfTI file
-modified_img = nib.Nifti1Image(final_image, img.affine, img.header)
-nib.save(modified_img, os.path.join(data_path, 'spine_cylinder.nii.gz'))
+nib.save(modified_file, os.path.join(data_path, 'spine_cylinder.nii.gz'))
