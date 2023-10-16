32e4230a7c538ad002f15b673cb2cee651780349
CereAle99 <alessandro@ceresi.eu>
2023-10-16 13:23:15 +0200

    add a main script moved from resizePET_Ale.py

---
+++ segmentation_processing/main.py
@@ -0,0 +1,60 @@
+import os
+
+if __name__ == "__main__":
+    # All patients path
+    current_directory = os.getcwd()
+    data_path = current_directory + '/data/test_PET'
+
+    for folder in os.listdir(data_path):
+        print("Patient: ", folder)
+        # Get label path
+        patient_path = os.path.join(data_path, folder)
+        image_path = os.path.join(patient_path, "PT.nii")
+
+        # Load label
+        img_obj = nib.load(image_path)
+        img = img_obj.get_fdata()
+        print("Initial shape: ", img.shape)
+        print("Old header: \n", img_obj.header)
+
+        # Evaluate the ratio of the images dimension
+        CT_PET_ratio = np.array([512, 512, 237]) / np.array(img.shape)
+
+        # Transform label
+        img = zoom(img, zoom=CT_PET_ratio)
+        print("Final shape: ", img.shape)
+
+        # Modify file header and affine
+        new_header = img_obj.header
+        new_header['dim'] = [3, 512, 512, 237, 1, 1, 1, 1]
+        new_header['pixdim'] = [
+                                1.,
+                                5.46875/CT_PET_ratio[0],
+                                5.46875/CT_PET_ratio[1],
+                                3.27/CT_PET_ratio[2],
+                                0.,
+                                0.,
+                                0.,
+                                0.
+                               ]
+        new_header['srow_x'] = [-5.46875/CT_PET_ratio[0], 0., 0., 347.26562]
+        new_header['srow_y'] = [0., -5.46875/CT_PET_ratio[1], 0., 347.26562]
+        new_header['srow_z'] = [0., 0., 3.27/CT_PET_ratio[2], -771.7]
+        new_affine = np.array(
+                              [
+                                [-5.46875/CT_PET_ratio[0], 0., 0., 347.26562],
+                                [0., -5.46875/CT_PET_ratio[1], 0., 347.26562],
+                                [0., 0., 3.27/CT_PET_ratio[2], -771.7],
+                                [0., 0., 0., 1.]
+                              ]
+                             )
+
+        img_resized = nib.Nifti1Image(img, new_affine, new_header)
+        print("New header: \n", img_resized.header)
+
+        # Save label
+        save_path = os.path.join(patient_path, "PET-Resized")
+        os.makedirs(save_path, exist_ok=True)
+        if os.path.isfile(save_path + '/PT.nii'):
+            print("Saving PET")
+            nib.save(img_resized, save_path + '/PT.nii')
