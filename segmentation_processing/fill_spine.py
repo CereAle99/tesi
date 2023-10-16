a25ca92dc8942d883883f2324c35187d8c3d52a2
CereAle99 <alessandro@ceresi.eu>
2023-10-16 15:06:56 +0200

    make the function work with the whole NIfTI file

--- segmentation_processing/fill_spine.py
+++ segmentation_processing/fill_spine.py
@@ -8,37 +8,38 @@ current_directory = os.getcwd()
 data_path = current_directory + '/data/'
 
 # Load the NIfTI segmentation and put it into a numpy array
-img = nib.load(data_path + "Spine.nii.gz")
-image = img.get_fdata()
+original_file = nib.load(data_path + "Spine.nii.gz")
 
 
 # Fill holes function
-
-def fill_spinal_holes(input_data, n_dilations=3, dim=3):
+def fill_spinal_holes(input_nifti, n_dilations=3, dim=3):
     """
 
     Args:
-        input_data:
+        input_nifti:
         dim:
         n_dilations:
 
     Returns:
 
     """
-    data = input_data
+
+    image = input_nifti.get_fdata()
     kernel = np.zeros((dim, dim, dim), dtype=np.uint8)
     kernel[:, dim // 2, :] = 1
 
-    data = ndimage.binary_dilation(data, iterations=n_dilations)
-    data = ndimage.binary_fill_holes(data, structure=kernel).astype(int)
-    final_data = ndimage.binary_erosion(data, iterations=n_dilations)
+    image = ndimage.binary_dilation(image, iterations=n_dilations)
+    image = ndimage.binary_fill_holes(image, structure=kernel).astype(int)
+    final_image = ndimage.binary_erosion(image, iterations=n_dilations)
 
-    return final_data
+    # Put the image in a NIfTI file
+    final_nifti = nib.Nifti1Image(final_image, input_nifti.affine, input_nifti.header)
+
+    return final_nifti
 
 
 # Fill the holes in the image
-final_image = fill_spinal_holes(image, 3)
+modified_file = fill_spinal_holes(original_file, 3)
 
 # Save the modified segmentation as a NIfTI file
-modified_img = nib.Nifti1Image(final_image, img.affine, img.header)
-nib.save(modified_img, os.path.join(data_path, 'show_results_n=0.nii.gz'))
+nib.save(modified_file, os.path.join(data_path, 'show_results_n=3.nii.gz'))
