6007955d3ca8949150c92b77b5ff07965b7f6158
CereAle99 <alessandro@ceresi.eu>
2023-10-17 10:27:33 +0200

    add algorithm that shapes the PET images

--- segmentation_processing/main.py
+++ segmentation_processing/main.py
@@ -1,60 +1,65 @@
 import os
+import nibabel as nib
+from crop_spine_from_PET import crop_spine_shape
 
 if __name__ == "__main__":
     # All patients path
-    current_directory = os.getcwd()
-    data_path = current_directory + '/data/test_PET'
+    moose_path1 = ("/run/user/1000/gvfs/afp-volume:host=RackStation.local,user=aceresi,"
+                   "volume=Genomed/Genomed4All_Data/MultipleMieloma/Segmentations/moose_1")
+    moose_path2 = ("/run/user/1000/gvfs/afp-volume:host=RackStation.local,user=aceresi,"
+                   "volume=Genomed/Genomed4All_Data/MultipleMieloma/Segmentations/moose_2")
+    patient_path = ("/run/user/1000/gvfs/afp-volume:host=RackStation.local,user=aceresi,"
+                    "volume=Genomed/Genomed4All_Data/MultipleMieloma/PET-CT")
+    label_folder = "labels/sim_space/similarity-space"
 
-    for folder in os.listdir(data_path):
+    # Get saving directory path
+    save_path = ("/run/user/1000/gvfs/afp-volume:host=RackStation.local,user=aceresi,"
+                 "volume=Genomed/Genomed4All_Data/MultipleMieloma/Segmentations")
+
+    # For each patient in folder moose_1
+    for folder in os.listdir(moose_path1):
         print("Patient: ", folder)
-        # Get label path
-        patient_path = os.path.join(data_path, folder)
-        image_path = os.path.join(patient_path, "PT.nii")
+
+        # Get label and PET path
+        patient_path = os.path.join(moose_path1, folder)
+        label_path = os.path.join(patient_path, "MOOSE-" + folder, label_folder, "Spine.nii.gz")
+        pet_path = os.path.join(patient_path, folder, "PT.nii")
 
         # Load label
-        img_obj = nib.load(image_path)
-        img = img_obj.get_fdata()
-        print("Initial shape: ", img.shape)
-        print("Old header: \n", img_obj.header)
+        segmentation_file = nib.load(label_path)
+        pet_file = nib.load(pet_path)
 
-        # Evaluate the ratio of the images dimension
-        CT_PET_ratio = np.array([512, 512, 237]) / np.array(img.shape)
+        # Shape the PET image
+        shapes = ["original", "fill_holes", "close_spine", "cylinder"]
+        for function in shapes:
+            if function == "original":
+                label = 41
+            else:
+                label = 1
+            cut_pet = crop_spine_shape(pet_file, segmentation_file, function, label)
+            nib.save(cut_pet, os.path.join(save_path, folder, 'PT_' + function + ".nii"))
 
-        # Transform label
-        img = zoom(img, zoom=CT_PET_ratio)
-        print("Final shape: ", img.shape)
-
-        # Modify file header and affine
-        new_header = img_obj.header
-        new_header['dim'] = [3, 512, 512, 237, 1, 1, 1, 1]
-        new_header['pixdim'] = [
-                                1.,
-                                5.46875/CT_PET_ratio[0],
-                                5.46875/CT_PET_ratio[1],
-                                3.27/CT_PET_ratio[2],
-                                0.,
-                                0.,
-                                0.,
-                                0.
-                               ]
-        new_header['srow_x'] = [-5.46875/CT_PET_ratio[0], 0., 0., 347.26562]
-        new_header['srow_y'] = [0., -5.46875/CT_PET_ratio[1], 0., 347.26562]
-        new_header['srow_z'] = [0., 0., 3.27/CT_PET_ratio[2], -771.7]
-        new_affine = np.array(
-                              [
-                                [-5.46875/CT_PET_ratio[0], 0., 0., 347.26562],
-                                [0., -5.46875/CT_PET_ratio[1], 0., 347.26562],
-                                [0., 0., 3.27/CT_PET_ratio[2], -771.7],
-                                [0., 0., 0., 1.]
-                              ]
-                             )
-
-        img_resized = nib.Nifti1Image(img, new_affine, new_header)
-        print("New header: \n", img_resized.header)
-
-        # Save label
-        save_path = os.path.join(patient_path, "PET-Resized")
-        os.makedirs(save_path, exist_ok=True)
-        if os.path.isfile(save_path + '/PT.nii'):
-            print("Saving PET")
-            nib.save(img_resized, save_path + '/PT.nii')
+    # Have to put a way to find the path of each file because moose 2.0 changes the folder name everytime
+    #
+    #
+    # for folder in os.listdir(moose_path2):
+    #     print("Patient: ", folder)
+    #
+    #     # Get label and PET path
+    #     patient_path = os.path.join(moose_path2, folder)
+    #     label_path = os.path.join(patient_path, "MOOSE-" + folder, label_folder, "Spine.nii.gz")
+    #     pet_path = os.path.join(patient_path, folder, "PT.nii")
+    #
+    #     # Load label
+    #     segmentation_file = nib.load(label_path)
+    #     pet_file = nib.load(pet_path)
+    #
+    #     # Shape the PET image
+    #     shapes = ["original", "fill_holes", "close_spine", "cylinder"]
+    #     for function in shapes:
+    #         if function == "original":
+    #             label = 41
+    #         else:
+    #             label = 1
+    #         cut_pet = crop_spine_shape(pet_file, segmentation_file, function, label)
+    #         nib.save(cut_pet, os.path.join(save_path, folder, 'PT_' + function + ".nii"))
