f186f6cd5d56e52b7a4b18b59cc5afee5ca9667e
CereAle99 <alessandro@ceresi.eu>
2023-11-20 13:53:02 +0100

    complete the code to extract and save the features

--- SimpleITK_feature_extractor.py
+++ SimpleITK_feature_extractor.py
@@ -1,4 +1,5 @@
 import numpy as np
+import pandas as pd
 import radiomics
 import SimpleITK as sitk
 import os
@@ -8,30 +9,47 @@ from radiomics import featureextractor, getTestCase
 
 if __name__ == "__main__":
 
-    # Get current path
+    # Get current path and all useful paths
     current_path = os.getcwd()
+    shared_dir_path = "/run/user/1000/gvfs/afp-volume:host=RackStation.local,user=aceresi,volume=Genomed"
+    segmentations_dir = shared_dir_path + "/Genomed4All_Data/MultipleMieloma/spine_PET/"
+    sick_patients_path = segmentations_dir + "sick_patients"
+    healthy_patients_path = segmentations_dir + "healthy_patients"
 
-    # Load the image
-    image_path = os.path.join(current_path, "data", "path_all_tua_immagine.nii")
-    imageName = sitk.ReadImage(image_path)
-    mask_path = os.path.join(current_path, "data", "path_all_tua_immagine.nii")
-    maskName = sitk.ReadImage(mask_path)
+    # Create the dataframe where to store all the features
 
-    # Set the features extractor
-    extr_params = os.path.join(current_path, "parameters", "feat_extr_test.yaml")
-    extractor = radiomics.featureextractor.RadiomicsFeatureExtractor(extr_params)
+    features = pd.DataFrame()
 
-    result = extractor.execute(imageName, maskName)
-    for key, val in six.iteritems(result):
-        print("\t%s: %s" % (key, val))
+    for patient_id in os.listdir(sick_patients_path):
+        print("Patient: ", patient_id)
 
-    result = extractor.execute(imageName, maskName, voxelBased=True)
-    for key, val in six.iteritems(result):
-        if isinstance(val, sitk.Image):  # Feature map
-            sitk.WriteImage(val, key + '.nrrd', True)
-            print("Stored feature %s in %s" % (key, key + ".nrrd"))
-        else:  # Diagnostic information
-            print("\t%s: %s" % (key, val))
+        # Select the pet images in patient folder
+        pet_files = [pet for pet in os.path.join(sick_patients_path, patient_id) if pet.startswith("PT")]
+
+        for pet_file in pet_files:
+
+            # Load the image and the segmentation
+            image_path = os.path.join(sick_patients_path, patient_id, pet_file)
+            imageName = sitk.ReadImage(image_path)
+            mask_path = os.path.join(sick_patients_path, patient_id, f"mask_{pet_file}")
+            maskName = sitk.ReadImage(mask_path)
+
+            # Set the features extractor
+            extr_params = os.path.join(current_path, "parameters", "feat_extr_test.yaml")
+            extractor = radiomics.featureextractor.RadiomicsFeatureExtractor(extr_params)
+
+            new_line = {"patient_id": patient_id, "mask_shape": pet_file[3:-7]}
+            result = extractor.execute(imageName, maskName)
+            for key, val in six.iteritems(result):
+                new_line[key] = val
+                print(f"Feature {key}: {val}")
+            features = features.append(new_line, ignore_index=True)
+            print("Stored features in ")
+            break
+            # result = extractor.execute(imageName, maskName, voxelBased=True)
+        break
+
+    features.to_csv(os.path.join(current_path, "data", "PT_rad_feats.csv"))
 
 
 
