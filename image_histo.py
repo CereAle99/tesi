4c47eb8979e5ee00ffe5c249bd2f1dacc7b7e2cb
CereAle99 <alessandro@ceresi.eu>
2023-11-21 17:09:24 +0100

    log scale and selected a PT image in the shared folder

--- image_histo.py
+++ image_histo.py
@@ -4,12 +4,12 @@ import matplotlib.pyplot as plt
 import os
 
 # Data paths
-shared_dir_path = "/run/user/1000/gvfs/afp-volume:host=RackStation.local,user=aceresi,volume=Genomed"
-data_directory = shared_dir_path + "/Genomed4All_Data/MultipleMieloma/spine_PET/sick_patients"
+shared_dir_path = "/run/user/1000/gvfs/smb-share:server=192.168.0.6,share=genomed"
+data_directory = shared_dir_path + "/Genomed4All_Data/MultipleMieloma/Original/PET-CT"
 
 # Image info to seek the path
 patient_id = "MPC_264_20160216"
-file_name = "CT_cylinder.nii.gz"
+file_name = "PT.nii"
 
 # Load the image
 file_path = os.path.join(shared_dir_path, data_directory, patient_id, file_name)
@@ -23,7 +23,8 @@ flat_data = ct_data.flatten()
 
 # Draw the histogram
 plt.hist(flat_data, bins=100, color='blue', edgecolor='black')
-plt.title('Grey levels histigram')
+plt.yscale('log')  # Imposta la scala logaritmica sull'asse y
+plt.title('Grey levels histogram')
 plt.xlabel('Grey levels')
 plt.ylabel('Frequency')
 plt.show()
