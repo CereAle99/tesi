1a31625f5371722165ecd2c0a604f784d81ef32f
CereAle99 <alessandro@ceresi.eu>
2023-11-23 13:26:25 +0100

    small optimizations

--- SimpleITK_feature_extractor.py
+++ SimpleITK_feature_extractor.py
@@ -16,7 +16,7 @@ if __name__ == "__main__":
     # Get current path and all useful paths
     current_path = os.getcwd()
     print(current_path)
-    shared_dir_path = "/run/user/1000/gvfs/smb-share:server=192.168.0.6,share=genomed"
+    shared_dir_path = "/run/user/1000/gvfs/afp-volume:host=RackStation.local,user=aceresi,volume=Genomed"
     segmentations_dir = shared_dir_path + "/Genomed4All_Data/MultipleMieloma/spine_PET/"
     sick_patients_path = segmentations_dir + "sick_patients"
     healthy_patients_path = segmentations_dir + "healthy_patients"
@@ -27,21 +27,22 @@ if __name__ == "__main__":
     for patient_id in os.listdir(sick_patients_path):
         print("Patient: ", patient_id)
 
+        # Set the features extractor
+        extr_params = os.path.join(current_path, "parameters", "feat_extr_test.yaml")
+        extractor = radiomics.featureextractor.RadiomicsFeatureExtractor(extr_params)
+
         try:
             # Select the pet images in patient folder
-            pet_files = [pet for pet in os.listdir(os.path.join(sick_patients_path, patient_id)) if pet.startswith("PT")]
+            pet_files = [pet for pet in os.listdir(os.path.join(sick_patients_path, patient_id)) if pet.startswith("PT_cy")]
 
             for pet_file in pet_files:
+                print(pet_file)
 
                 # Load the image and the segmentation
                 image_path = os.path.join(sick_patients_path, patient_id, pet_file)
                 imageName = sitk.ReadImage(image_path)
                 mask_path = os.path.join(sick_patients_path, patient_id, f"mask_{pet_file}")
                 maskName = sitk.ReadImage(mask_path)
-
-                # Set the features extractor
-                extr_params = os.path.join(current_path, "feat_extr_test.yaml")
-                extractor = radiomics.featureextractor.RadiomicsFeatureExtractor(extr_params)
 
                 # Add new line in features dataset
                 new_line = {"patient_id": patient_id, "mask_shape": pet_file[3:-7]}
@@ -61,8 +62,9 @@ if __name__ == "__main__":
         except Exception as e:
             print(f"Unknown error ({e}) for patient: ", patient_id)
             # Write the patient id which had an error
-            with open(current_path + "/feat_extr_log.txt", "a") as file:
+            with open(current_path + "/log/feat_extr_log.txt", "a") as file:
                 file.write(f"{patient_id}:  ({e})\n")
 
     # Features dataframe save
-    feats_dataframe.to_csv(os.path.join(sick_patients_path, "PT_rad_feats.csv"))
+    feats_dataframe.to_csv(os.path.join(current_path, "log", "PT_rad_feats.csv"))
+    
