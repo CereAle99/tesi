7b6beb28ef7eadfe53dd9b1e9ce8509add47a33c
CereAle99 <alessandro@ceresi.eu>
2023-11-27 01:48:21 +0100

    get just the patients directory

--- delete_in_dir.py
+++ delete_in_dir.py
@@ -10,16 +10,16 @@ if __name__ == "__main__":
 
     # # Checkpoint last execution
     next_start = "MPC_2710_20070302"
-    patients = os.listdir(sick_patients_path)
+    patients = [patient for patient in os.listdir(sick_patients_path) if patient.startswith("MPC")]
     index = patients.index(next_start)
 
     # Loop for all the patients
     for patient_id in patients:
-        print(patient_id, ".  Pregresses: ", patients.index(patient_id)/len(patients))
+        print(patient_id, ".  Pregresses: ", patients.index(patient_id)/len(patients) * 100)
         patient_dir = os.path.join(sick_patients_path, patient_id)
         
         # List of paths towards files to remove with a specific start
-        files_toremove = [file for file in os.listdir(patient_dir) if file.startswith("~")]
+        files_toremove = [file for file in os.listdir(patient_dir) if file.startswith("~g")]
         
         # Loop through all the files to remove
         for file in files_toremove:
