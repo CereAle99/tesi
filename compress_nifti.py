1c268db29bc3a4bf5a2aa8f839a4871f427a7d7f
CereAle99 <alessandro@ceresi.eu>
2023-11-29 17:38:12 +0100

    modify for compress healthy nifti

--- compress_nifti.py
+++ compress_nifti.py
@@ -9,19 +9,18 @@ if __name__ == "__main__":
     moose_path1 = shared_dir_path + "/Genomed4All_Data/MultipleMieloma/Original/Moose_output/moose_1"
     moose_path2 = shared_dir_path + "/Genomed4All_Data/MultipleMieloma/Original/Moose_output/moose_2"
     data_path = shared_dir_path + "/Genomed4All_Data/MultipleMieloma/Original/PET-CT"
-    save_path = shared_dir_path + "/Genomed4All_Data/MultipleMieloma/spine_PET/sick_patients"
-    # save_path = os.path.join(current_path, "data", "test_PET")
+    save_path = shared_dir_path + "/Genomed4All_Data/MultipleMieloma/spine_PET/healthy_patients"
 
     # For each patient in folder moose_1
     for patient_id in os.listdir(save_path):
         print("Patient: ", patient_id)
         try:
-            dir_sick_patient = os.path.join(save_path, patient_id)
-            if os.path.isdir(dir_sick_patient):
-                nifti_files = [file_name for file_name in os.listdir(dir_sick_patient) if file_name.endswith(".nii")]
+            dir_patient = os.path.join(save_path, patient_id)
+            if os.path.isdir(dir_patient):
+                nifti_files = [file_name for file_name in os.listdir(dir_patient) if file_name.endswith(".nii")]
                 print(f"List of files to converted in the folder {patient_id}: {nifti_files}")
                 for file in nifti_files:
-                    file_path = os.path.join(dir_sick_patient, file)
+                    file_path = os.path.join(dir_patient, file)
                     print("\nPath towards the NIfTI file:  ", file_path)
                     nifti = nib.load(file_path)
                     print("load")
