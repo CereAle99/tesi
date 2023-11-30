5705e1044fa760978aaf68d96f7a10f0a1d6e303
CereAle99 <alessandro@ceresi.eu>
2023-11-30 12:46:37 +0100

    add code to get healthy patients with more than one set of images

---
+++ count_multifile_dir.py
@@ -0,0 +1,30 @@
+import os
+import csv
+
+
+if __name__ == "__main__":
+    
+    # Get all useful paths
+    shared_dir_path = "/run/user/1000/gvfs/smb-share:server=192.168.0.6,share=genomed"
+    segmentations_dir = shared_dir_path + "/Genomed4All_Data/MultipleMieloma/spine_PET/"
+    healthy_patients_path = segmentations_dir + "healthy_patients"
+
+    patients = os.listdir(healthy_patients_path)
+
+    ambiguous_segm = []
+
+    for patient in patients:
+
+        patient_dirs = [dir for dir in os.listdir(os.path.join(healthy_patients_path, patient)) if os.path.isdir(dir)]
+
+        if enumerate(patient_dirs) > 1:
+            
+            ambiguous_segm.append(patient)
+    
+    with open("healthy_ambiguous_segm.txt", "a", newline="") as csv_file:
+
+        # create writer
+        csv_writer = csv.writer(csv_file)
+
+        # Add listto csv file
+        csv_writer.writerow(ambiguous_segm)
