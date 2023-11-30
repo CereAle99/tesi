aca6b96cbf75202218568ce9475f8201e206a537
CereAle99 <alessandro@ceresi.eu>
2023-11-30 13:00:31 +0100

    fix the path

--- count_multifile_dir.py
+++ count_multifile_dir.py
@@ -6,8 +6,7 @@ if __name__ == "__main__":
     
     # Get all useful paths
     shared_dir_path = "/run/user/1000/gvfs/smb-share:server=192.168.0.6,share=genomed"
-    segmentations_dir = shared_dir_path + "/Genomed4All_Data/MultipleMieloma/spine_PET/"
-    healthy_patients_path = segmentations_dir + "healthy_patients"
+    healthy_patients_path = shared_dir_path + "/Genomed4All_Data/MultipleMieloma/Healthy/HEALTHY-PET-CT/FDG-PET-CT-Lesions"
 
     # Get patients folders list
     patients = os.listdir(healthy_patients_path)
@@ -20,16 +19,18 @@ if __name__ == "__main__":
     for patient in patients:
         
         # list of dirs in patient folder
-        patient_dirs = [dir for dir in os.listdir(os.path.join(healthy_patients_path, patient)) if os.path.isdir(dir)]
+        patient_dir = os.path.join(healthy_patients_path, patient)
+        image_sets = [dir for dir in os.listdir(patient_dir) if os.path.isdir(os.path.join(patient_dir, dir))]
+        print(image_sets)
 
         # add to the list the patient id if patient_dirs is > 1
-        if len(patient_dirs) > 1:
+        if len(image_sets) > 1:
             
             ambiguous_segm.append(patient)
             print(patient)
 
     # Save the patient id list
-    with open("healthy_ambiguous_segm.txt", "a", newline="") as csv_file:
+    with open("healthy_ambiguous_segm.csv", "a", newline="") as csv_file:
 
         # create writer
         csv_writer = csv.writer(csv_file)
