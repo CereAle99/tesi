4ef271f7bdd443fc494cd4931cc21a3655b97b6c
CereAle99 <alessandro@ceresi.eu>
2023-12-05 09:55:38 +0100

    delete file .nii in healthy segmentations folders

--- delete_in_dir.py
+++ delete_in_dir.py
@@ -6,12 +6,12 @@ if __name__ == "__main__":
     # Get all useful paths
     shared_dir_path = "/run/user/1000/gvfs/smb-share:server=192.168.0.6,share=genomed"
     segmentations_dir = shared_dir_path + "/Genomed4All_Data/MultipleMieloma/spine_PET/"
-    sick_patients_path = segmentations_dir + "sick_patients"
+    sick_patients_path = segmentations_dir + "healthy_patients"
 
     # # Checkpoint last execution
-    next_start = "MPC_2710_20070302"
-    patients = [patient for patient in os.listdir(sick_patients_path) if patient.startswith("MPC")]
-    index = patients.index(next_start)
+    # next_start = "MPC_2710_20070302"
+    patients = [patient for patient in os.listdir(sick_patients_path) if patient.startswith("PETCT")]
+    # index = patients.index(next_start)
 
     # Loop for all the patients
     for patient_id in patients:
@@ -19,7 +19,7 @@ if __name__ == "__main__":
         patient_dir = os.path.join(sick_patients_path, patient_id)
         
         # List of paths towards files to remove with a specific start
-        files_toremove = [file for file in os.listdir(patient_dir) if file.startswith("~g")]
+        files_toremove = [file for file in os.listdir(patient_dir) if file.endswith(".nii")]
         
         # Loop through all the files to remove
         for file in files_toremove:
